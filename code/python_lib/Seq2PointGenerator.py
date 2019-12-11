355c55fd5c59a74cf978a7314476e60fa73a7c0e
FarmerRobbie <farmerrobbie@freshsalad.today>
2019-12-11 13:45:43 -0800

    working on datagenerator.

--- code/python_lib/Seq2PointGenerator.py
+++ code/python_lib/Seq2PointGenerator.py
@@ -20,18 +20,24 @@ class Seq2PointGenerator(keras.utils.Sequence):
         self.inputs, self.targets = np_array[:, 0], np_array[:, 1]
         self.inputs = K.cast_to_floatx(self.inputs)
         self.targets = K.cast_to_floatx(self.targets)
+        self.offset = int(0.5*(self.windowsize-1.0))
+        self.max_batchsize = self.inputs.size - 2 * self.offset
 
     def __len__(self):
-        return int(np.ceil(len(self.inputs)/self.batchsize))
+        a = int(np.ceil(len(self.inputs)/self.batchsize))
+        return a
 
     def __getitem__(self, index):
         # excerpt gets us batchsize rows.  Each row has windowsize number of readings.  The readings are in order
         # The researchers note "sliding window" here where I think of it more as a random place to start grabbing
         # a windowsize bunch of readings.
+        indexes = np.arange(self.max_batchsize)
+        if self.shuffle:
+            np.random.shuffle(indexes)
         for start_idx in range(0, self.max_batchsize, self.batchsize):
-            excerpt = indices[start_idx:start_idx + self.batchsize]
+            excerpt = indexes[start_idx:start_idx + self.batchsize]
             # The input is a 2D array where number of rows = batchsize and number of cols = windowsize
-            inp = np.array([inputs[idx:idx + self.windowsize]
+            inp = np.array([self.inputs[idx:idx + self.windowsize]
                             for idx in excerpt])
             # ---------------------
             # From the paper: "...the output is the midpoint element x of the corresponding
@@ -44,7 +50,7 @@ class Seq2PointGenerator(keras.utils.Sequence):
             # The entry is the midpoint of the targets for the inputs (mains electricity) womdpw
             # of data.
             #
-            tar = targets[excerpt + self.offset].reshape(-1, 1)
+            tar = self.targets[excerpt + self.offset].reshape(-1, 1)
             # The inp numpy array has batchsize rows.  Each row has window size number of readings.
             # The tar numpy array is the center point value of the target column within the window size
             # number of aggregate readings of the inp array.
@@ -55,14 +61,12 @@ class Seq2PointGenerator(keras.utils.Sequence):
 # Triggered once at the beginning as well as at the end of each epoch.
 #
 
-    def on_epoch_end(self):
-        self.offset = int(0.5*(self.windowsize-1.0))
-        self.max_batchsize = self.inputs.size - 2 * offset
-        if self.batchsize < 0:
-            self.batchsize = max_batchsize
-        self.indexes = np.arange(max_batchsize)
-        # From https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
-        # Shuffling the order in which examples are fed to the classifier is helpful so that
-        # batches between epochs do not look alike. Doing so will eventually make our model more robust.
-        if self.shuffle:
-            np.random.shuffle(self.indexes)
+    # def on_epoch_end(self):
+    #     self.offset = int(0.5*(self.windowsize-1.0))
+    #     self.max_batchsize = self.inputs.size - 2 * self.offset
+    #     self.indexes = np.arange(self.max_batchsize)
+    #     # From https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
+    #     # Shuffling the order in which examples are fed to the classifier is helpful so that
+    #     # batches between epochs do not look alike. Doing so will eventually make our model more robust.
+    #     if self.shuffle:
+    #         np.random.shuffle(self.indexes)
--- code/seq2pointgenerator_test.py
+++ code/seq2pointgenerator_test.py
@@ -1,9 +1,23 @@
 from python_lib.Seq2PointGenerator import Seq2PointGenerator
 import pandas as pd
 import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Reshape
+
 
 a = np.arange(20)
 a = a.reshape((10, 2))
 df = pd.DataFrame(a)
 df.columns = ['aggregate', 'microwave']
-gen = Seq2PointGenerator(df,windowsize=3, batchsize=5)
+batchsize = 5
+windowsize = 3
+gen = Seq2PointGenerator(df, windowsize=windowsize, batchsize=batchsize)
+model = Sequential()
+# The inp numpy array has batchsize rows.  Each row has window size number of readings.
+
+# model.add(Reshape((-1, windowsize, 1), input_shape=(1, windowsize)))
+model.add(Dense(1, activation='relu', input_shape=(windowsize,)))
+model.summary()
+model.compile(loss='mean_squared_error',
+              optimizer='adam', metrics=['accuracy'])
+model.fit_generator(generator=gen)
